35616dd72c103f1101b12f2ac4db8b2fd563b60d
\Student 6 <Student 6@10.8.33.169>
2016-09-15 13:14:42 -0400

    Jillian says her name check this out

--- HansIntroduction/src/introUnit/ProceduralExample.java
+++ HansIntroduction/src/introUnit/ProceduralExample.java
@@ -12,6 +12,8 @@ public class ProceduralExample {
 	//this is another comment
 	//one more comment for the road
 	public static void main(String[] args){
-	  System.out.println("Hello World");		
+		//System.out.println("Hello World");
+		String[] names = {"Jillian","Joseph","Jordan"};
+		System.out.println("My name is "+names[0]+".");
 	}
 }
