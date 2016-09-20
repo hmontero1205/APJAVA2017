057b8b429214c5fc564172563e972b1024e70550
\Student 6 <Student 6@10.8.33.169>
2016-09-20 12:32:26 -0400

    Math Random Exercise!!

---
+++ HansIntroduction/src/datatypes/MathRandom.java
@@ -0,0 +1,24 @@
+package datatypes;
+
+import java.util.Random;
+
+public class MathRandom {
+
+	public static void main(String[] args) {
+		
+		for(int index = 0; index < 10; index++){
+			System.out.println("Roll #"+(index+1)+" Die is "+rollFairDie());
+		}
+
+	}
+	
+	public static int rollFairDie(){
+		Random gen = new Random();
+		return gen.nextInt(6)+1;
+	}
+	
+	public static int rollUnfairDie(){
+		
+	}
+
+}
