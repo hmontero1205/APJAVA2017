aaf3a604562cdda9a1ffe92b16f10e0cc50fb237
\Student 6 <Student 6@10.8.33.169>
2016-09-20 12:21:41 -0400

    some cool memory examples

--- HansIntroduction/src/datatypes/DataTypes.java
+++ HansIntroduction/src/datatypes/DataTypes.java
@@ -1,4 +1,4 @@
-package datatypes;
+ package datatypes;
 
 public class DataTypes {
 
@@ -7,10 +7,22 @@ public class DataTypes {
 		double d2 = 2.0;
 		double d3 = 2.64;
 
-		System.out.println("d1 : " + d1); 
-		System.out.println("d2 : " + d2); 
-		System.out.println("d3 : " + d3);  
-		System.out.println("d1 - d2 : " + (float)(d1 - d2));
+//		System.out.println("d1 : " + d1); 
+//		System.out.println("d2 : " + d2); 
+//		System.out.println("d3 : " + d3);  
+//		System.out.println("d1 - d2 : " + (float)(d1 - d2));
+		
+////		String s = ""+1;
+////		System.out.println(s);
+////		
+////		
+////		String ten = "10";
+////		String two = "2";
+////		
+////		if(ten.compareTo(two)<0){
+////			System.out.println("10 is greater than 2!");
+////			//1 comes in alpha order before 2!!
+//		}
 	}
 
 }
