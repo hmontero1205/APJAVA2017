45ad1361ecddfabd605a97bdeb44311303e0c736
\Student 6 <Student 6@10.8.33.169>
2016-09-22 12:34:03 -0400

    stuff

--- HansIntroduction/src/datatypes/StringPractice.java
+++ HansIntroduction/src/datatypes/StringPractice.java
@@ -1,8 +1,50 @@
 package datatypes;
 
+import java.util.Scanner;
+
 public class StringPractice {
+	static Scanner input;
+	static String user;
+	static int lineCount;
 
 	public static void main(String[] args) {
+		createAScanner();
+		lineCount = 0;
+		//demonstrateStringMethods();
+		//promptInput();
+		promptName();
+		talkForever();
+		
+	}
+	
+	private static void promptName() {
+		print("Suh, dude. I am a board covered with semiconductors and other electronic components. What is your name?");
+		user = input.nextLine();
+		print("Cool beans. I will call you "+user+" until you terminate me.");
+	}
+
+	public static void talkForever(){
+		while(true){
+			promptInput();
+		}
+	}
+
+	public static void promptInput() {
+		print(user+", try inputting a string!");
+		String userInput = input.nextLine();
+		print("You typed: "+userInput);
+		
+	}
+
+	public static void createAScanner() {
+		input = new Scanner(System.in);
+		
+	}
+	public static void print(String s){
+		lineCount++;
+		System.out.println("Line #"+lineCount+": "+s);
+	}
+	public static void demonstrateStringMethods(){
 		//String text = new String("Hello World!");
 		String text1 = "Hello World!";
 		String text2 = "Hello ";
