84fcf2e7f7c57b60fec1b3a5b371cd0b3cd9f676
\Student 6 <Student 6@10.8.33.169>
2016-10-11 12:58:56 -0400

    initialized

---
+++ Classwork/src/array/ArraysMain.java
@@ -0,0 +1,13 @@
+package array;
+
+public class ArraysMain {
+
+	public static void main(String[] args) {
+		int[] zeros1 = {0,0,0};
+		int[] zeros2 = new int[3];
+		String[] strings1 = {"","",""};
+		String[] strings2 = new String[3];//full of nulls
+		
+	}
+
+}
