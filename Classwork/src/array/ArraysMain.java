f5b2c8d2e76da5d3351c21e304da741299650ab7
\Student 6 <Student 6@10.8.33.169>
2016-10-11 13:08:45 -0400

    initialization

--- Classwork/src/array/ArraysMain.java
+++ Classwork/src/array/ArraysMain.java
@@ -8,6 +8,17 @@ public class ArraysMain {
 		String[] strings1 = {"","",""};
 		String[] strings2 = new String[3];//full of nulls
 		
+		boolean[] booleans =  new boolean[3];
+		//index of data is important to reference.
+		for(int index=0;index<booleans.length;index++){
+			System.out.println(index+") "+booleans[index]);
+		}
+		//always goes in order, doesnt go in order
+		int index2 = 0;
+		for(boolean b:booleans){
+			System.out.println(index2+") "+b);
+			index2++;
+		}
 	}
 
 }
