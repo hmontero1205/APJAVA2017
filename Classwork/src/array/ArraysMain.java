f56f4d4092180eaec176223177b683a63a34f4db
\Student 6 <Student 6@10.8.33.169>
2016-10-11 13:17:13 -0400

    timing the computer

--- Classwork/src/array/ArraysMain.java
+++ Classwork/src/array/ArraysMain.java
@@ -3,12 +3,26 @@ package array;
 public class ArraysMain {
 
 	public static void main(String[] args) {
+		//timing how quickly a computer processes.
+		long startTime = System.currentTimeMillis();
+		arrayIntroMethod();
+		long endTime = System.currentTimeMillis();
+		System.out.println("Completed method in..."+(endTime-startTime)+"ms!!");
+	}
+	public static void arrayIntroMethod(){
 		int[] zeros1 = {0,0,0};
 		int[] zeros2 = new int[3];
 		String[] strings1 = {"","",""};
 		String[] strings2 = new String[3];//full of nulls
 		
-		boolean[] booleans =  new boolean[3];
+		for(String s1: strings1){
+			System.out.println(s1);
+		}
+		for(String s2: strings2){
+			System.out.println(s2);
+		}
+		
+		/*boolean[] booleans =  new boolean[3];
 		//index of data is important to reference.
 		for(int index=0;index<booleans.length;index++){
 			System.out.println(index+") "+booleans[index]);
@@ -18,7 +32,6 @@ public class ArraysMain {
 		for(boolean b:booleans){
 			System.out.println(index2+") "+b);
 			index2++;
-		}
+		}*/
 	}
-
 }
